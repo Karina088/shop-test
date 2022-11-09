e32c5425e242bc9ac1e5ddbeae6b742f54cf12ae
kary_ser <morskya.life@gmail.com>
2022-11-09 15:16:08 +0300

    updated main.js, written with classes

--- new_shop/main.js
+++ new_shop/main.js
@@ -1,32 +1,76 @@
 'use strict';
-// при выводе массива по умолчанию запускается метод join, который выводит запятые. Если ничего не хочу видеть при выводе массива клиенту ставля пустые кавычки join(''). Если я хочу выводить с новой строки - join('\n')
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Mouse', price: 20 },
-    { id: 3, title: 'Keyboard', price: 200 },
-    { id: 4, title: 'Gamepad', price: 50 },
-];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
 
-// product - это все параметры объектов из массива products
-const renderProduct = (product, img = 'https://via.placeholder.com/200x150') => {
-    return `<div class="product-item">
-                <img scr="${img}">
-                <h3>${product.title}</h3>
-                <p class="product__text">${product.price}$</p>
+// найти стоимость всех товаров
+// спроектировать классы корзина и товаров корзинки, заполнить их свойствами и методами -пустые
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.fetchProducts(); //рекомендация, чтобы метод был вызван в текущем классе
+        this.render(); //вывод товаров на страницу
+    }
+
+    getSum() {
+
+        // найти стоимость всех товаров this.goods через forEach
+    }
+
+    fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+            // block.innerHTML += item.render();
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p class="product__text">${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
+    }
+}
 
-const renderPage = list => {
-    document.querySelector('.products').innerHTML =
-        (list.map(product => renderProduct(product))).join('');
-};
+let list = new ProductList();
 
-renderPage(products);
 
-    // быстрее будет innerHTML, тк innerHTML() нужен, когда есть цикл
-    // const renderPage = list => {
-    //     document.querySelector('.products').innerHTML('beforeend', list.map(product => renderProduct(product)).join(''));
-    // };
+// const products = [
+//     { id: 1, title: 'Notebook', price: 2000 },
+//     { id: 2, title: 'Mouse', price: 20 },
+//     { id: 3, title: 'Keyboard', price: 200 },
+//     { id: 4, title: 'Gamepad', price: 50 },
+// ];
+// const renderProduct = (product, img = 'https://via.placeholder.com/200x150') => {
+//     return `<div class="product-item">
+//                 <img scr="${img}">
+//                 <h3>${product.title}</h3>
+//                 <p class="product__text">${product.price}$</p>
+//                 <button class="buy-btn">Купить</button>
+//             </div>`
+// };
+// const renderPage = list => {
+//     document.querySelector('.products').innerHTML =
+//         (list.map(product => renderProduct(product))).join('');
+// };
+// renderPage(products);
+
