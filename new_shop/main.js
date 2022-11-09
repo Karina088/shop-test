1ce4d05c8e87e3a8e8546cdc92d26f9ff6974cfe
kary_ser <morskya.life@gmail.com>
2022-11-09 17:33:24 +0300

    correct output of goods, added basket classes and basket elements with methods

--- new_shop/main.js
+++ new_shop/main.js
@@ -1,21 +1,14 @@
 'use strict';
 
-// найти стоимость всех товаров
-// спроектировать классы корзина и товаров корзинки, заполнить их свойствами и методами -пустые
 class ProductList {
     constructor(container = '.products') {
         this.container = container;
         this.goods = [];
-        this.fetchProducts(); //рекомендация, чтобы метод был вызван в текущем классе
-        this.render(); //вывод товаров на страницу
+        this.allProducts = [];
+        this._fetchProducts(); //рекомендация, чтобы метод был вызван в текущем классе
     }
 
-    getSum() {
-
-        // найти стоимость всех товаров this.goods через forEach
-    }
-
-    fetchProducts() {
+    _fetchProducts() {
         this.goods = [
             { id: 1, title: 'Notebook', price: 2000 },
             { id: 2, title: 'Mouse', price: 20 },
@@ -27,23 +20,34 @@ class ProductList {
     render() {
         const block = document.querySelector(this.container);
         for (let product of this.goods) {
-            const item = new ProductItem(product);
-            block.insertAdjacentHTML("beforeend", item.render());
-            // block.innerHTML += item.render();
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML("beforeend", productObj.render());
         }
+    }
+
+    getSum() {
+        // let sum = 0;
+        // this.goods.forEach(item => {
+        //     sum += item.price;
+        // });
+        // reduce используется для последовательной обработки каждого элемента массива с сохранением промежуточного результата
+        let result = this.allProducts.reduce((acc, item) => acc + item.price, 0);
+        console.log(result);
+
     }
 }
 
 class ProductItem {
     constructor(product, img = 'https://via.placeholder.com/200x150') {
         this.title = product.title;
-        this.id = product.id;
         this.price = product.price;
+        this.id = product.id;
         this.img = img;
     }
     render() {
-        return `<div class="product-item">
-                <img src="${this.img}">
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="photo">
                 <h3>${this.title}</h3>
                 <p class="product__text">${this.price}</p>
                 <button class="buy-btn">Купить</button>
@@ -52,7 +56,30 @@ class ProductItem {
 }
 
 let list = new ProductList();
+list.render();
+list.getSum();
 
+class Basket {
+
+    addGood() {
+
+    }
+    removeGood() {
+
+    }
+
+    changeGood() {
+
+    }
+
+    render() {
+
+    }
+}
+
+class ElemBasket {
+    render() { }
+}
 
 // const products = [
 //     { id: 1, title: 'Notebook', price: 2000 },
